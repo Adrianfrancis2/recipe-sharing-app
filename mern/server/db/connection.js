d8017b3cc2b8db19d6b11a8e5d0da072fc3a7fe5
Adrianfrancis2 <adrianfrancisrozario@gmail.com>
2024-11-02 17:19:19 -0700

    project files added

---
+++ mern/server/db/connection.js
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  //  Connect client to the server
+  await client.connect();
+  //  Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+    "Successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("recipes");
+
+export default db;
---
+++ mern/server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import recipes from "./routes/recipe.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/recipe", recipes);
+
+//  Start Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
