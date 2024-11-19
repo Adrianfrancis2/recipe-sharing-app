5179574eab24d50ccc510aae74335d5eb3b21769
bry4b <116151219+bry4b@users.noreply.github.com>
2024-11-18 17:07:08 -0800

    prettied userProfile page

--- mern/client/src/components/UserProfile.jsx
+++ mern/client/src/components/UserProfile.jsx
@@ -39,10 +39,31 @@ export default function UserProfile() {
       }
     
       return (
-        <div>
-          <h1>User Profile</h1>
-          <p><strong>Name:</strong> {user.name}</p>
-          <p><strong>Username:</strong> {user.username}</p>
+        <div className="border rounded-lg overflow-hidden p-4">
+
+          <div className="grid grid-cols-1 gap-x-8 gap-y-2 border-b border-slate-900/10 pb-12 md:grid-cols-2">
+            <div className="flex items-center space-x-1">
+              <h3 className="text-lg font-semibold">
+                User Profile
+              </h3>
+            </div>
+            <div className="flex items-center space-x-1">
+                <h2 className="text-base font-semibold leading-7 text-slate-900">
+                  Name:
+                </h2>  
+                <span className="text-base text-slate-900" >
+                  {user.name}
+                </span>
+            </div>
+            <div className="flex items-center space-x-1">
+                <h2 className="text-base font-semibold leading-7 text-slate-900">
+                  Username:
+                </h2>  
+                <span className="text-base text-slate-900" >
+                  {user.username}
+                </span>
+            </div>
+          </div>
         </div>
       );
     }
