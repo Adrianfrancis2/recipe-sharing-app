9f7a136b1d472bda197ac73f6301f22d2e946d39
bry4b <116151219+bry4b@users.noreply.github.com>
2024-11-21 00:35:51 -0800

    minor tweak to hide password in LoginUser

--- mern/client/src/components/LoginUser.jsx
+++ mern/client/src/components/LoginUser.jsx
@@ -123,7 +123,7 @@ export default function LoginUser({ setLogin }) {
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
-                    type="text"
+                    type="password"
                     name="password"
                     id="password"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
